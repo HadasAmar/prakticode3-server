53532b36218731c4f3503f7dc46fbf613ff4a232
HadasAmar <hadas7260@gmail.com>
2025-03-23 16:59:53 +0200

    update docker

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b66a2af54df1cea91decde9179e82cf4fa22519c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25f6caf1cc7f419f2f02b8bb54dd71874cad28ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
